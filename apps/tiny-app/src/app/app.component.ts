70c5e2c0cbe4395c896fdb51e8b85e28620cc501
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-03-21 22:47:44 +0100

    refactor: switch to SCSS

--- apps/tiny-app/src/app/app.component.ts
+++ apps/tiny-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nx-tiny-app-project-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'tiny-app';
