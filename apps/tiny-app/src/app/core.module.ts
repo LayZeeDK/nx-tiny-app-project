fffc07eb58e3c4c9fffa937a7a34712800c436cc
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-03-23 00:51:25 +0100

    refactor: exctract NgRx Store configuration

--- apps/tiny-app/src/app/core.module.ts
+++ apps/tiny-app/src/app/core.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '@workspace/shared/environments';
-
-import { metaReducers, reducers } from './reducers';
+import { SharedDataAccessModule } from '@workspace/shared/data-access';
 
 @NgModule({
   imports: [
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
-    StoreDevtoolsModule.instrument({
-      logOnly: environment.production,
-      maxAge: 25,
-    }),
+    SharedDataAccessModule.forRoot(),
   ],
 })
 export class CoreModule {}
--- libs/shared/data-access/src/lib/shared-data-access.module.ts
+++ libs/shared/data-access/src/lib/shared-data-access.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '@workspace/shared/environments';
+
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+    }),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production,
+      maxAge: 25,
+    }),
+  ],
 })
-export class SharedDataAccessModule {}
+export class SharedDataAccessRootModule {}
+
+@NgModule({})
+export class SharedDataAccessModule {
+  static forRoot(): ModuleWithProviders<SharedDataAccessRootModule> {
+    return {
+      ngModule: SharedDataAccessRootModule,
+    };
+  }
+}
