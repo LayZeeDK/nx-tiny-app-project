e088accbb201475432de17b90dd093d5ac584f12
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-03-23 01:24:21 +0100

    fix: enable debug meta reducer in development mode

--- libs/shared/data-access/src/lib/reducers/index.ts
+++ libs/shared/data-access/src/lib/reducers/index.ts
@@ -8,4 +8,4 @@ export interface State {}
 export const reducers: ActionReducerMap<State> = {};
 
 export const metaReducers: MetaReducer<State>[] =
-  !environment.production ? [] : [debug];
+  !environment.production ? [debug] : [];
